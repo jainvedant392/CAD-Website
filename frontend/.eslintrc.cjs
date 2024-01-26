467dbc79fa0bc70e1c2a9b7d12da8a01b3e0771f
jainvedant392 <jainvedant392@gmail.com>
2024-01-26 22:30:07 +0530

    repository setup

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb/hooks', 'eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/no-unescaped-entities': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+function App() {
+  return (
+    <MantineProvider>
+      <h1>cad website.</h1>
+    </MantineProvider>
+  );
+}
+
+export default App;
